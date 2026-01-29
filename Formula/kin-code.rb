b88210376532c54312b275f0c4cf797ad137dd2f
blake-gj <blake-gj@users.noreply.github.com>
2026-01-29 01:56:47 +0000

    Update kin-code to 1.1.1

--- Formula/kin-code.rb
+++ Formula/kin-code.rb
@@ -3,8 +3,8 @@ class KinCode < Formula
 
   desc "AI-powered coding assistant CLI"
   homepage "https://github.com/kinra-ai/kin-code"
-  url "https://files.pythonhosted.org/packages/source/k/kin-code/kin_code-1.1.0.tar.gz"
-  sha256 "de0e76006a92f5f8ba02d62e2bf3f83bbab6fc230bb18a88dd6b9e7bc533360e"
+  url "https://files.pythonhosted.org/packages/source/k/kin-code/kin_code-1.1.1.tar.gz"
+  sha256 "0cbac67702b336e5ae822adb6a467a51766ce9f79dd38893ea934e724b2121c4"
   license "Apache-2.0"
 
   depends_on "python@3.12"
